3e0491e02c4a8fd2da3d3b84a42d35386d0291fd
Ar4ka <rene.juergens01@gmail.com>
2023-05-07 15:02:01 +0200

    zsh configs

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# Alias
+# ---
+#
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
+
+
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
+alias vim=nvim
