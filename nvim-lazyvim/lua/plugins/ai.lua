10a94500842382fe6e2e89e71a1dbd0d9cd296d5
Ar4ka <rene.juergens01@gmail.com>
2023-10-10 16:56:06 +0200

    chore: removed chatGPT

--- nvim-lazyvim/lua/plugins/ai.lua
+++ nvim-lazyvim/lua/plugins/ai.lua
@@ -153,19 +153,4 @@ return {
       })
     end,
   },
-
-  -- https://github.com/jackMort/ChatGPT.nvim
-  {
-    "jackMort/ChatGPT.nvim",
-    dependencies = {
-      { "MunifTanjim/nui.nvim" },
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
-    config = function()
-      require("chatgpt").setup({
-        -- optional configuration
-      })
-    end,
-  },
 }
