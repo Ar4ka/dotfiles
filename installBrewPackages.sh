b65f2c73a22da9a856cbe6afb17dcf40a2ab0a89
Ar4ka <rene.juergens01@gmail.com>
2023-11-14 11:38:30 +0100

    feat: added install for brew packages used

---
+++ installBrewPackages.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install bash-completion
+brew install btop
+brew install cmake
+brew install cocoapods
+brew install corepack
+brew install direnv
+brew install exa
+brew install fastlane
+brew install gh
+brew install git
+brew install git-lfs
+brew install gnu-sed
+brew install gnupg
+brew install goto
+brew install imagemagick
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+brew install lazygit
+brew install lazydocker
+brew install nano
+brew install nanorc
+brew install neofetch
+brew install neovim
+brew install nvm
+brew install protobuf
+brew install python
+brew install python@3.10
+brew install ripgrep
+brew install rust
+brew install starship
+brew install tmux
+brew install tmux-mem-cpu-load
+brew install wget
+brew install wix/brew/applesimutils
+brew install zplug
