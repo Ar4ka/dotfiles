cfe1dee06030faba147d0aeb21cf880ca4b62419
Ar4ka <rene.juergens01@gmail.com>
2023-11-20 14:18:09 +0100

    feat: some neo-tree settings

--- nvim-lazyvim/lua/plugins/filetree.lua
+++ nvim-lazyvim/lua/plugins/filetree.lua
@@ -1,6 +1,6 @@
 return {
 
-  -- change neo-tree config
+  -- change neo-tree confi
   {
     "nvim-neo-tree/neo-tree.nvim",
     -- opts will be merged with the parent spec
@@ -15,6 +15,9 @@ return {
         -- This will use the OS level file watchers to detect changes
         -- instead of relying on nvim autocmd events.
         use_libuv_file_watcher = true,
+      },
+      window = {
+        position = "right",
       },
     },
   },
