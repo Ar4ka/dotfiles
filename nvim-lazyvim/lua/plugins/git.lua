83c5a5c59b9b474c27f9efde9d346d3fc5b16a05
Ar4ka <rene.juergens01@gmail.com>
2024-02-14 14:03:00 +0100

    remove: octo.nvim

--- nvim-lazyvim/lua/plugins/git.lua
+++ nvim-lazyvim/lua/plugins/git.lua
@@ -65,17 +65,6 @@ return {
   },
 
   {
-    "pwntester/octo.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-  },
-  {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
