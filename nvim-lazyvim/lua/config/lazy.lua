4a02af78745487cefb43df0803fcebd626561a8c
Ar4ka <rene.juergens01@gmail.com>
2024-05-17 15:32:34 +0200

    fix(lazyvim): autocomplete not working

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
 
     { import = "plugins" },
